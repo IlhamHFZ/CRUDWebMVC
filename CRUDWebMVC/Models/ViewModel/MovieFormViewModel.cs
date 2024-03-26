dacbabcf2f04a0b0191fa3ac6ef26a13dbc365d0
Ilham Hafidz Fahry <ilhamhafidzfahry@gmail.com>
2024-03-26 22:28:46 +0700

    add MovieViewModel

---
+++ CRUDWebMVC/Models/ViewModel/MovieFormViewModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using CRUDWebMVC.Models.Entity;
+
+namespace CRUDWebMVC.Models.ViewModel;
+
+public class MovieFormViewModel
+{
+	public Guid? Id {get; set;}
+	
+	[Required(ErrorMessage = "Name is Required")]
+	[Display(Name = "Name")]
+	public string Name {get; set;} = null!;
+	
+	[Required(ErrorMessage = "Release Date is Required")]
+	[Display(Name = "Release Date")]
+	public DateTime ReleaseDate {get; set;}
+	
+	[Required(ErrorMessage = "Date Added is Required")]
+	[Display(Name = "Date of Added is Required")]
+	public DateTime DateAdded {get; set;}
+	
+	[Required(ErrorMessage = "Stock is Required")]
+	[Range(0, int.MaxValue, ErrorMessage = "stock must be large than 0")]
+	[Display(Name = "Stock")]
+	public int Stock {get; set;}
+	
+	[Display(Name = "Genre")]
+	public IEnumerable<Genre> Genres {get; set;} = null!;
+	
+}
